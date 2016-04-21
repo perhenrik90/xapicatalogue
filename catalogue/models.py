f2e4b6bfcb69956fa545055d42a5324f7bac531b
Per-Henrik Kvalnes <per-henrik.kvalnes@nlsh.no>
2016-04-21 10:18:18 +0200

    added two data models

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class LRS(models.Model):
+
+    url = models.TextField(max_length=300)
+    local_name = models.TextField(max_length=30)
+
+    username = models.TextField(max_length=100)
+    key = models.TextField(max_length=100)
+
+
+class xResource(models.Model):
+
+    url = models.TextField(max_length=300)
+    name = models.TextField(max_length=80)
+    description = models.TextField(max_length=400)
+    
