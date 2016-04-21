3e09f65ea986a1f4d53f1e29df1deac120a9930f
Per-Henrik Kvalnes <per-henrik.kvalnes@nlsh.no>
2016-04-21 11:01:33 +0200

    added first view and template

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.translation import ugettext as _
+from django.core.urlresolvers import reverse
+from django.template import RequestContext, loader, Template, Context
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def view_all_resources(request):
+
+    c ={}
+    
+    template = loader.get_template("all_resources.html")
+    context = RequestContext(request, c)
+    return HttpResponse(template.render(context))
--- xapicatalogue/urls.py
+++ xapicatalogue/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'xapicatalogue.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'catalogue.views.view_all_resources'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
