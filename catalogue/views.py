a74b1c21e4d226a36aff2f744bd08699ac44efa4
Per-Henrik Kvalnes <per-henrik.kvalnes@nlsh.no>
2016-09-09 14:28:29 +0200

    changed lrs url creator to work with Learning Locker

--- catalogue/views.py
+++ catalogue/views.py
@@ -19,7 +19,8 @@ def view_all_resources(request):
     c ={}
     
     user = request.user
-
+    safe = '~()*!.\''
+    
     # get the first LRS resource
     lrs = LRS.objects.all()[0]
     c["lrs"] = lrs
@@ -27,9 +28,10 @@ def view_all_resources(request):
 
     # setup xapi actor
     actor = {}
-    actor["name"] = user.first_name + " " + user.last_name
-    actor["mbox"] = "mailto:"+user.email
+    actor['name'] = user.first_name + " " + user.last_name
+    actor['mbox'] = "mailto:"+user.email
     tcactor = json.dumps(actor)
+    print(tcactor)
 
     for resource in resources:
 
@@ -37,22 +39,28 @@ def view_all_resources(request):
         tckey = lrs.key
         tcurl = lrs.url
 
-        pair = b''+(tcusername+":"+tckey)
-        tchash = base64.b64encode(pair.encode('latin1'))
+        pair = u''+(tcusername+":"+tckey)
+        tchash = base64.b64encode(pair)
         
         url = resource.url + "?"
         urlparam = {}
 
-        urlparam["tincan"] = True
+        urlparam["tincan"] = "true"
         urlparam["endpoint"]=tcurl
-        urlparam["auth"] = "Basic "+tchash
-        #urlparam["actor"] = tcactor
-        
-        # urlparam += u"&activity_id="+urllib.quote("www.example.com/completed")
-        urlparam["ampregistration"] = "2981c910-6445-11e4-9803-0800200c9a36"
-        print(urllib.urlencode(urlparam))
-        resource.tcurl = url+ urllib.urlencode(urlparam)
+        urlparam["auth"] = urllib.quote("Basic "+tchash)
+        urlparam["actor"] = urllib.quote(tcactor, safe)
 
+        urlpara = "?tincan=true&"
+        urlpara += "endpoint="+urlparam["endpoint"]
+        urlpara += "&auth="+urlparam["auth"]
+        urlpara += "&actor="+urlparam["actor"]
+
+        print("\n\n"+urlpara)
+        urlparam["activity_id"] = urllib.quote("www.example.com/course")
+        urlparam["registration"] = urllib.quote("2981c910-6445-11e4-9803-0800200c9a36")
+
+        resource.tcurl = url+ urlpara
+                
 
 
 
